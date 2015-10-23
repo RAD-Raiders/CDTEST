77a297d9e52046455d20802b79566b4fb96ebc43
unknown <koshya@R9032HM0.corp.root.nasd.com>
2015-10-23 08:52:22 -0400

    updated sh to rb file

---
+++ after_deploy.rb
@@ -0,0 +1,22 @@
+require "open-uri"
+require 'json'
+data = open("http://finracrd.elasticbeanstalk.com/").read
+puts data
+build = JSON.parse(data)
+curr_id =  build[0]["buildid"]
+t = Time.now
+s_time = t.strftime "%Y-%m-%d-&h%m%s"
+id =  (curr_id.to_i + 1).to_s
+puts "http://finracrd.elasticbeanstalk.com/results/#{id}/integration/PASS"
+data = open("http://finracrd.elasticbeanstalk.com/results/#{id}/integration/PASS").read
+=begin
+uri = URI('http://finracrd.elasticbeanstalk.com/')
+req = Net::HTTP::Post.new(uri)
+req.set_form_data('buildid' => curr_id + 1)
+#res = Net::HTTP.post_form(uri, 'buildid' => curr_id+1)
+
+
+Net::HTTP.new('http://finracrd.elasticbeanstalk.com/').start { |http|
+	http.request(req)
+}
+=end
