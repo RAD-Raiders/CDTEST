074ef9fa8c73fd1c9e033636282de859028f3dd1
unknown <KoshyA@R9032HM0.corp.root.nasd.com>
2015-10-24 17:29:47 -0400

    updated smoke

--- aws_deploy.rb
+++ aws_deploy.rb
@@ -1,5 +1,6 @@
 require 'open-uri'
 require 'json'
+require 'net/ping'
 data = open("http://finracrd.elasticbeanstalk.com/").read
 puts data
 build = JSON.parse(data)
@@ -7,6 +8,21 @@ curr_id =  build[0]["buildid"]
 id =  (curr_id.to_i).to_s
 puts "http://finracrd.elasticbeanstalk.com/results/#{id}/health/PASS"
 data = open("http://finracrd.elasticbeanstalk.com/results/#{id}/health/PASS").read
+
+
+def up?(host)
+    check = Net::Ping::External.new(host)
+    check.ping?
+end
+
+chost = 'http://rails-env-dev-jmzm5tk3fp.elasticbeanstalk.com/'
+if up?(chost) # returns true if ping replies
+	puts "http://finracrd.elasticbeanstalk.com/results/#{id}/smoke/PASS"
+	data = open("http://finracrd.elasticbeanstalk.com/results/#{id}/smoke/FAIL").read
+else
+	puts "http://finracrd.elasticbeanstalk.com/results/#{id}/smoke/PASS"
+    data = open("http://finracrd.elasticbeanstalk.com/results/#{id}/smoke/FAIL").read
+end
 =begin
 uri = URI('http://finracrd.elasticbeanstalk.com/')
 req = Net::HTTP::Post.new(uri)
